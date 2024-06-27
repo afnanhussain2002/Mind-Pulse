0c0861aaf71ae737947670bb1f239dd7b39765c5
afnanhussain2002 <afnanhussain2022@gmail.com>
2024-06-27 09:13:30 +0600

    Client: Add complete responsive header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -5,6 +5,7 @@ import Button from "./Button";
 import MenuSvg from "../assets/svg/MenuSvg";
 import { HamburgerMenu } from "./design/Header";
 import { useState } from "react";
+import { disablePageScroll, enablePageScroll } from 'scroll-lock';
 
 const Header = () => {
   const pathname = useLocation();
@@ -13,11 +14,14 @@ const Header = () => {
   const toggleMobileMenu = () => {
     if (openNavigation) {
       setOpenNavigation(false);
+      enablePageScroll()
     } else {
       setOpenNavigation(true);
+      disablePageScroll()
     }
   };
   const handleClick = () => {
+    if(!openNavigation) return;
     setOpenNavigation(false);
   };
   return (
